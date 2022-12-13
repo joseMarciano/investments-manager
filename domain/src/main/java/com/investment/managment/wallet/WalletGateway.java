c9737260ad762d5f160028c26a445c0f97d9cfa6
joseMarciano <marcianojosepaulo@gmail.com>
2022-12-13 20:57:00 -0300

    chore: :wrench: create findALl in WalletGateway

--- domain/src/main/java/com/investment/managment/wallet/WalletGateway.java
+++ domain/src/main/java/com/investment/managment/wallet/WalletGateway.java
@@ -1,9 +1,16 @@
 package com.investment.managment.wallet;
 
+import com.investment.managment.page.Pagination;
+import com.investment.managment.page.SearchQuery;
+
 import java.util.Optional;
 
 public interface WalletGateway {
     Wallet create(Wallet aWallet);
+
     Wallet update(Wallet aWallet);
+
     Optional<Wallet> findById(WalletID anId);
+
+    Pagination<Wallet> findAll(SearchQuery query);
 }
