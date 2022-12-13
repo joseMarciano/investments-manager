7e100a4d4176756d79af27e57b0476ed7c70a78e
joseMarciano <marcianojosepaulo@gmail.com>
2022-12-12 21:59:32 -0300

    chore: :wrench: create update and findById in WalletGateway

--- domain/src/main/java/com/investment/managment/wallet/WalletGateway.java
+++ domain/src/main/java/com/investment/managment/wallet/WalletGateway.java
@@ -1,5 +1,9 @@
 package com.investment.managment.wallet;
 
+import java.util.Optional;
+
 public interface WalletGateway {
     Wallet create(Wallet aWallet);
+    Wallet update(Wallet aWallet);
+    Optional<Wallet> findById(WalletID anId);
 }
