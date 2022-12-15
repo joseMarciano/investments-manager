f93e5ac0134307bd9d65906aedbb47fba9de8030
joseMarciano <marcianojosepaulo@gmail.com>
2022-12-14 23:35:57 -0300

    chore: :wrench: create a integration test annotation

---
+++ infrastructure/src/test/java/com/investment/managment/IntegrationTest.java
@@ -0,0 +1,14 @@
+package com.investment.managment;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@SpringBootTest(classes = App.class)
+public @interface IntegrationTest {
+}
