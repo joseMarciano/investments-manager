96b3adc58dd65b430ecb0a2c9f6a4fc23a700425
joseMarciano <marcianojosepaulo@gmail.com>
2022-12-17 10:04:15 -0300

    test: :test_tube: create test about update Wallet of WalletGateway

--- infrastructure/src/test/java/com/investment/managment/wallet/persistence/WalletGatewayIT.java
+++ infrastructure/src/test/java/com/investment/managment/wallet/persistence/WalletGatewayIT.java
@@ -37,7 +37,9 @@ public class WalletGatewayIT extends DataBaseExtension {
                 .color(expectedColor)
                 .build();
 
+        Assertions.assertEquals(0, walletRepository.count());
         final Wallet actualWallet = walletGateway.create(aWallet);
+        Assertions.assertEquals(1, walletRepository.count());
 
         Assertions.assertNotNull(aWallet.getId());
         Assertions.assertEquals(aWallet.getId(), actualWallet.getId());
@@ -58,6 +60,55 @@ public class WalletGatewayIT extends DataBaseExtension {
         Assertions.assertEquals(walletJpaEntity.getCreatedAt().toString(), actualWallet.getUpdatedAt().toString());
         Assertions.assertEquals(walletJpaEntity.getCreatedAt().toString(), actualWallet.getCreatedAt().toString());
         Assertions.assertEquals(walletJpaEntity.getUpdatedAt().toString(), actualWallet.getUpdatedAt().toString());
+        Assertions.assertEquals(1, walletRepository.count());
+
+    }
+
+    @Test
+    public void givenAValidWallet_whenCallsUpdate_shouldUpdateIt() {
+        final var expectedName = "Wallet";
+        final var expectedDescription = "Wallet long term";
+        final var expectedColor = "FFFFF";
+        var aWallet = WalletBuilder.create()
+                .name("wallet")
+                .description("Wallet term")
+                .color("FFEFF")
+                .build();
+        final var expectedId = aWallet.getId();
+
+        Assertions.assertEquals(0, walletRepository.count());
+        var walletJPAEntity = walletRepository.saveAndFlush(WalletJpaEntity.from(aWallet));
+        Assertions.assertEquals(1, walletRepository.count());
+
+        Assertions.assertNotNull(aWallet.getId());
+        Assertions.assertEquals(aWallet.getId().getValue(), walletJPAEntity.getId());
+        Assertions.assertEquals(aWallet.getDescription(), walletJPAEntity.getDescription());
+        Assertions.assertEquals(aWallet.getColor(), walletJPAEntity.getColor());
+        Assertions.assertNotNull(aWallet.getCreatedAt());
+        Assertions.assertEquals(aWallet.getCreatedAt(), walletJPAEntity.getUpdatedAt());
+        Assertions.assertEquals(aWallet.getCreatedAt(), walletJPAEntity.getCreatedAt());
+        Assertions.assertEquals(aWallet.getUpdatedAt(), walletJPAEntity.getUpdatedAt());
+
+        aWallet.update(expectedName, expectedDescription, expectedColor);
+        final var walletUpdated = walletGateway.update(aWallet);
+
+        Assertions.assertNotNull(walletUpdated.getId());
+        Assertions.assertEquals(walletUpdated.getId(), expectedId);
+        Assertions.assertEquals(walletUpdated.getDescription(), expectedDescription);
+        Assertions.assertEquals(walletUpdated.getColor(), expectedColor);
+        Assertions.assertNotNull(walletUpdated.getCreatedAt());
+        Assertions.assertNotNull(walletUpdated.getUpdatedAt());
+        Assertions.assertTrue(walletUpdated.getCreatedAt().isBefore(walletUpdated.getUpdatedAt()));
+
+        final var actualWallet = walletRepository.findById(aWallet.getId().getValue()).get().toAggregate();
+        Assertions.assertNotNull(actualWallet.getId());
+        Assertions.assertEquals(actualWallet.getId(), expectedId);
+        Assertions.assertEquals(actualWallet.getDescription(), expectedDescription);
+        Assertions.assertEquals(actualWallet.getColor(), expectedColor);
+        Assertions.assertNotNull(actualWallet.getCreatedAt());
+        Assertions.assertNotNull(actualWallet.getUpdatedAt());
+        Assertions.assertTrue(actualWallet.getCreatedAt().isBefore(actualWallet.getUpdatedAt()));
+
     }
 
 
