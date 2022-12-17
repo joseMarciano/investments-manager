6437b0782682adbaa54b1e9d3709dce2c6cd3d04
joseMarciano <marcianojosepaulo@gmail.com>
2022-12-16 23:32:54 -0300

    feat: :sparkles: create Implementation about create of WalletGateway

--- domain/src/main/java/com/investment/managment/wallet/WalletID.java
+++ domain/src/main/java/com/investment/managment/wallet/WalletID.java
@@ -3,12 +3,18 @@ package com.investment.managment.wallet;
 import com.investment.managment.Identifier;
 import com.investment.managment.util.IdUtil;
 
+import java.util.Objects;
+
 public class WalletID extends Identifier<String> {
 
     private final String id;
 
     public static WalletID unique() {
         return new WalletID(IdUtil.unique());
+    }
+
+    public static WalletID from(final String aValue) {
+        return new WalletID(aValue);
     }
 
     private WalletID(final String id) {
@@ -18,5 +24,18 @@ public class WalletID extends Identifier<String> {
     @Override
     public String getValue() {
         return this.id;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final WalletID walletID = (WalletID) o;
+        return id.equals(walletID.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
