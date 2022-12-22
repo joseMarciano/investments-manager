11c19a345927c962a265da2b1e4d0cff1f16a5ef
joseMarciano <marcianojosepaulo@gmail.com>
2022-12-22 20:20:29 -0300

    chore: :wrench: create E2E test annotation

---
+++ infrastructure/src/test/java/com/investment/managment/E2ETest.java
@@ -0,0 +1,19 @@
+package com.investment.managment;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@SpringBootTest(classes = App.class)
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+public @interface E2ETest {
+}
+
