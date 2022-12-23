9c9197da9c9b71e2a9bf5744147a2e5a250d5cb4
joseMarciano <marcianojosepaulo@gmail.com>
2022-12-22 23:43:12 -0300

    chore: :wrench: add internal server error handler

--- infrastructure/src/main/java/com/investment/managment/api/ResponseExceptionHandler.java
+++ infrastructure/src/main/java/com/investment/managment/api/ResponseExceptionHandler.java
@@ -3,15 +3,24 @@ package com.investment.managment.api;
 import com.investment.managment.validation.exception.DomainException;
 import com.investment.managment.validation.exception.Error;
 import com.investment.managment.validation.exception.NotFoundException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.List;
 
 @ControllerAdvice
 public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<?> internalServerError(final Exception ex, final WebRequest webRequest) {
+        return handleExceptionInternal(ex, new ApiError("Internal server error",
+                List.of(new Error(ex.getMessage()))), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, webRequest);
+    }
 
     @ExceptionHandler(value = {DomainException.class})
     public ResponseEntity<?> domainException(final DomainException domainException) {
