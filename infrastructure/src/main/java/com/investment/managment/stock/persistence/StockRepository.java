555561368b2e623117236c5b9cdde0a591c7f61a
joseMarciano <marcianojosepaulo@gmail.com>
2023-01-04 20:00:11 -0300

    feat: :sparkles: implements findBySymbol

--- infrastructure/src/main/java/com/investment/managment/stock/persistence/StockRepository.java
+++ infrastructure/src/main/java/com/investment/managment/stock/persistence/StockRepository.java
@@ -3,5 +3,9 @@ package com.investment.managment.stock.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
+
 public interface StockRepository extends JpaRepository<StockJpaEntity, String>, JpaSpecificationExecutor<StockJpaEntity> {
+
+    Optional<StockJpaEntity> findBySymbol(String symbol);
 }
