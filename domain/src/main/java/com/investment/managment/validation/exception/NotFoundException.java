3a2c502f96d008502fdfc7a39b46c5f680a3d999
joseMarciano <marcianojosepaulo@gmail.com>
2023-01-07 19:57:28 -0300

    chore: :wrench: put null safe in Identifier

--- domain/src/main/java/com/investment/managment/validation/exception/NotFoundException.java
+++ domain/src/main/java/com/investment/managment/validation/exception/NotFoundException.java
@@ -5,6 +5,8 @@ import com.investment.managment.Identifier;
 
 import java.util.List;
 
+import static java.util.Optional.ofNullable;
+
 public class NotFoundException extends DomainException {
     private NotFoundException(final Error anError) {
         super(List.of(anError));
@@ -12,6 +14,6 @@ public class NotFoundException extends DomainException {
     }
 
     public static NotFoundException of(final Identifier<?> anId, final Class<? extends AggregateRoot<?>> aClass) {
-        return new NotFoundException(new Error("Entity %s with identifier %s was not found".formatted(aClass.getSimpleName(), anId.getValue())));
+        return new NotFoundException(new Error("Entity %s with identifier %s was not found".formatted(aClass.getSimpleName(), ofNullable(anId).map(Identifier::getValue).orElse(null))));
     }
 }
