5be4468a8f9074139970fd4ec9e67ac0bb810f1b
joseMarciano <marcianojosepaulo@gmail.com>
2023-01-11 20:54:47 -0300

    refactor: :hammer: change strategy of update execution use case

--- application/src/main/java/com/investment/managment/execution/update/UpdateExecutionUseCase.java
+++ application/src/main/java/com/investment/managment/execution/update/UpdateExecutionUseCase.java
@@ -4,11 +4,12 @@ import com.investment.managment.UseCase;
 import com.investment.managment.execution.Execution;
 import com.investment.managment.execution.ExecutionGateway;
 import com.investment.managment.execution.ExecutionID;
+import com.investment.managment.execution.update.buy.UpdateBuyFieldsExecutionUseCase;
+import com.investment.managment.execution.update.sell.UpdateSellFieldsExecutionUseCase;
 import com.investment.managment.stock.Stock;
 import com.investment.managment.stock.StockGateway;
 import com.investment.managment.stock.StockID;
 
-import static com.investment.managment.execution.ExecutionStatus.BUY;
 import static com.investment.managment.execution.ExecutionStatus.SELL;
 import static com.investment.managment.validation.exception.DomainExeceptionFactory.notFoundException;
 import static java.util.Objects.requireNonNull;
@@ -19,11 +20,19 @@ public class UpdateExecutionUseCase extends UseCase<UpdateExecutionCommandInput,
 
     private final StockGateway stockGateway;
 
+    private final UpdateBuyFieldsExecutionUseCase updateBuyFieldsExecutionUseCase;
+
+    private final UpdateSellFieldsExecutionUseCase updateSellFieldsExecutionUseCase;
+
 
     public UpdateExecutionUseCase(final ExecutionGateway executionGateway,
-                                  final StockGateway stockGateway) {
+                                  final StockGateway stockGateway,
+                                  final UpdateBuyFieldsExecutionUseCase updateBuyFieldsExecutionUseCase,
+                                  final UpdateSellFieldsExecutionUseCase updateSellFieldsExecutionUseCase) {
         this.executionGateway = requireNonNull(executionGateway);
         this.stockGateway = requireNonNull(stockGateway);
+        this.updateBuyFieldsExecutionUseCase = updateBuyFieldsExecutionUseCase;
+        this.updateSellFieldsExecutionUseCase = updateSellFieldsExecutionUseCase;
     }
 
     @Override
@@ -37,19 +46,25 @@ public class UpdateExecutionUseCase extends UseCase<UpdateExecutionCommandInput,
         final var anExecution = this.executionGateway.findById(executionID)
                 .orElseThrow(() -> notFoundException(executionID, Execution.class));
 
+        if (SELL.equals(anExecution.getStatus())) return this.updateSellFieldsExecutionUseCase.execute(aCommand);
 
-        if (BUY.equals(anExecution.getStatus())) {
-            updateBUYFields(aCommand, anExecution);
-        }
-
-        if (SELL.equals(anExecution.getStatus())) {
-            updateSELLFields(aCommand, anExecution);
-        }
-
-
-        return UpdateExecutionCommandOutput.from(executionGateway.update(anExecution));
+        return this.updateBuyFieldsExecutionUseCase.execute(aCommand);
     }
 
+
+    private void resolveToBuyStatus(final UpdateExecutionCommandInput aCommand, final Execution anExecution) {
+        final ExecutionID executionID = anExecution.getId();
+
+        if (!this.executionGateway.existsByOriginId(executionID)) {
+            updateBUYFields(aCommand, anExecution);
+            return;
+        }
+
+
+        this.executionGateway.findAllByOriginId(executionID);
+
+
+    }
 
     private void updateBUYFields(final UpdateExecutionCommandInput aCommand, final Execution anExecution) {
         anExecution
