53f27afec4c435e35bf9bcd22abc7e2995d77a8d
joseMarciano <marcianojosepaulo@gmail.com>
2023-01-10 21:24:01 -0300

    chore: :wrench: create use case beans

---
+++ infrastructure/src/main/java/com/investment/managment/config/execution/ExecutionUseCaseConfig.java
@@ -0,0 +1,35 @@
+package com.investment.managment.config.execution;
+
+import com.investment.managment.execution.ExecutionGateway;
+import com.investment.managment.execution.create.CreateExecutionUseCase;
+import com.investment.managment.execution.update.UpdateExecutionUseCase;
+import com.investment.managment.stock.StockGateway;
+import com.investment.managment.wallet.WalletGateway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ExecutionUseCaseConfig {
+
+    private final ExecutionGateway executionGateway;
+    private final StockGateway stockGateway;
+    private final WalletGateway walletGateway;
+
+    public ExecutionUseCaseConfig(final ExecutionGateway executionGateway,
+                                  final StockGateway stockGateway,
+                                  final WalletGateway walletGateway) {
+        this.executionGateway = executionGateway;
+        this.stockGateway = stockGateway;
+        this.walletGateway = walletGateway;
+    }
+
+    @Bean
+    public CreateExecutionUseCase createExecutionUseCase() {
+        return new CreateExecutionUseCase(this.executionGateway, this.stockGateway, this.walletGateway);
+    }
+
+    @Bean
+    public UpdateExecutionUseCase updateExecutionUseCase() {
+        return new UpdateExecutionUseCase(this.executionGateway, this.stockGateway);
+    }
+}
