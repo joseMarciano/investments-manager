67bf0a3340b4c5920f5cff3c56b3a96783611c93
joseMarciano <marcianojosepaulo@gmail.com>
2023-01-11 23:07:25 -0300

    chore: :wrench: create methods in exectution gateway

--- domain/src/main/java/com/investment/managment/execution/ExecutionGateway.java
+++ domain/src/main/java/com/investment/managment/execution/ExecutionGateway.java
@@ -3,6 +3,7 @@ package com.investment.managment.execution;
 import com.investment.managment.page.Pagination;
 import com.investment.managment.page.SearchQuery;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ExecutionGateway {
@@ -13,6 +14,12 @@ public interface ExecutionGateway {
     Optional<Execution> findById(ExecutionID anId);
 
     Pagination<Execution> findAll(SearchQuery query);
+
+    List<Execution> findAllByOriginId(ExecutionID originId);
+
+    boolean existsByOriginId(ExecutionID... originId);
+
+    boolean existsById(ExecutionID... originId);
 
     void deleteById(ExecutionID id);
 }
