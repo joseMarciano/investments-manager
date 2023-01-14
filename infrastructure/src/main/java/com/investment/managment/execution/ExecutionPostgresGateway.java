cdacfb73c2ea9a483bc944ddc7d2e80fe4e41ac5
joseMarciano <marcianojosepaulo@gmail.com>
2023-01-14 15:03:16 -0300

    feat: :sparkles: implements findAllByOriginId and existsByOriginId

--- infrastructure/src/main/java/com/investment/managment/execution/ExecutionPostgresGateway.java
+++ infrastructure/src/main/java/com/investment/managment/execution/ExecutionPostgresGateway.java
@@ -6,7 +6,11 @@ import com.investment.managment.page.Pagination;
 import com.investment.managment.page.SearchQuery;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Component
 public class ExecutionPostgresGateway implements ExecutionGateway {
@@ -30,6 +34,20 @@ public class ExecutionPostgresGateway implements ExecutionGateway {
     @Override
     public Optional<Execution> findById(final ExecutionID anId) {
         return this.executionRepository.findById(anId.getValue()).map(ExecutionJpaEntity::toAggregate);
+    }
+
+    @Override
+    public List<Execution> findAllByOriginId(final ExecutionID origin) {
+        return this.executionRepository.findByOrigin_Id(origin.getValue()).stream().map(ExecutionJpaEntity::toAggregate).toList();
+    }
+
+    @Override
+    public boolean existsByOriginId(final ExecutionID... originIds) {
+        final var ids =
+                Optional.ofNullable(originIds)
+                        .map(it -> Arrays.stream(it).map(ExecutionID::getValue).collect(Collectors.toSet()))
+                        .orElse(Collections.emptySet());
+        return this.executionRepository.existsByOrigin_IdIn(ids);
     }
 
     @Override
