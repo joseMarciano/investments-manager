d8f06b0c3e05b15f6a621c9476301f8ee6eb1200
joseMarciano <marcianojosepaulo@gmail.com>
2023-01-14 15:03:40 -0300

    chore: :wrench: create existsByOrigin_IdIn

--- infrastructure/src/main/java/com/investment/managment/execution/persistence/ExecutionRepository.java
+++ infrastructure/src/main/java/com/investment/managment/execution/persistence/ExecutionRepository.java
@@ -3,5 +3,11 @@ package com.investment.managment.execution.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+import java.util.Set;
+
 public interface ExecutionRepository extends JpaRepository<ExecutionJpaEntity, String>, JpaSpecificationExecutor<ExecutionJpaEntity> {
+    boolean existsByOrigin_IdIn(Set<String> ids);
+
+    List<ExecutionJpaEntity> findByOrigin_Id(String id);
 }
