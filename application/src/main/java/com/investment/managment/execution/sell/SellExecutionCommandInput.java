f36b34e302073b84254c67b4b71922b3ccf1074a
joseMarciano <marcianojosepaulo@gmail.com>
2023-01-17 19:09:37 -0300

    feat: :sparkles: implements sell execution use case

---
+++ application/src/main/java/com/investment/managment/execution/sell/SellExecutionCommandInput.java
@@ -0,0 +1,24 @@
+package com.investment.managment.execution.sell;
+
+import com.investment.managment.execution.ExecutionID;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+public record SellExecutionCommandInput(
+        ExecutionID originId,
+        Long executedQuantity,
+        BigDecimal executedPrice,
+        Instant executedAt
+
+) {
+
+    public static SellExecutionCommandInput with(
+            final ExecutionID originId,
+            final Long executedQuantity,
+            final BigDecimal executedPrice,
+            final Instant executedAt) {
+        return new SellExecutionCommandInput(originId, executedQuantity, executedPrice, executedAt);
+    }
+
+}
---
+++ application/src/main/java/com/investment/managment/execution/sell/SellExecutionUseCase.java
@@ -0,0 +1,66 @@
+package com.investment.managment.execution.sell;
+
+import com.investment.managment.UseCase;
+import com.investment.managment.execution.Execution;
+import com.investment.managment.execution.ExecutionBuilder;
+import com.investment.managment.execution.ExecutionGateway;
+import com.investment.managment.execution.ExecutionStatus;
+import com.investment.managment.validation.exception.DomainExeceptionFactory;
+import com.investment.managment.validation.exception.Error;
+
+import java.util.Objects;
+import java.util.function.Function;
+
+import static java.util.Objects.requireNonNull;
+
+public class SellExecutionUseCase extends UseCase<SellExecutionCommandInput, SellExecutionCommandOutput> {
+
+    private final ExecutionGateway executionGateway;
+
+    public SellExecutionUseCase(final ExecutionGateway executionGateway) {
+        this.executionGateway = requireNonNull(executionGateway);
+    }
+
+    @Override
+    public SellExecutionCommandOutput execute(final SellExecutionCommandInput aCommand) {
+        final var originId = aCommand.originId();
+        return this.executionGateway.findById(originId)
+                .map(resolve(aCommand))
+                .orElseThrow(() -> DomainExeceptionFactory.notFoundException(originId, Execution.class));
+    }
+
+    private Function<Execution, SellExecutionCommandOutput> resolve(final SellExecutionCommandInput aCommand) {
+        return originExecution -> {
+            final var executionSold = ExecutionBuilder.create()
+                    .status(ExecutionStatus.SELL)
+                    .executedQuantity(aCommand.executedQuantity())
+                    .executedPrice(aCommand.executedPrice())
+                    .executedAt(aCommand.executedAt())
+                    .profitPercentage(originExecution.getProfitPercentage())
+                    .stockId(originExecution.getStockId())
+                    .walletId(originExecution.getWalletId())
+                    .origin(originExecution.getId())
+                    .build();
+
+            validate(originExecution, aCommand);
+
+            return SellExecutionCommandOutput.from(this.executionGateway.create(executionSold));
+        };
+    }
+
+    private void validate(final Execution originExecution, final SellExecutionCommandInput aCommand) {
+        final var totalExecutedQuantity = originExecution.getExecutedQuantity();
+        final var currentExecutionsSold = this.executionGateway.findAllByOriginId(originExecution.getId()) // TODO: generify validators in application layer
+                .stream()
+                .map(Execution::getExecutedQuantity)
+                .filter(Objects::nonNull)
+                .reduce(Long::sum)
+                .orElse(0L);
+        final var quantityRemaining = totalExecutedQuantity - currentExecutionsSold;
+
+        if (quantityRemaining < aCommand.executedQuantity()) {
+            throw DomainExeceptionFactory.constraintException(new Error("'executedQuantity' can not be greater than %s".formatted(quantityRemaining)));
+        }
+
+    }
+}
