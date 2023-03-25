9564c37ce0a7daad9bdbe637dacaa5db3d2b63cb
joseMarciano <marcianojosepaulo@gmail.com>
2023-03-25 16:26:07 -0300

    fix: :bug: fix @SQSListener that wasn't work

--- infrastructure/src/main/java/com/investment/managment/App.java
+++ infrastructure/src/main/java/com/investment/managment/App.java
@@ -1,10 +1,21 @@
 package com.investment.managment;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableAutoConfiguration(
+        exclude = {
+                ContextInstanceDataAutoConfiguration.class,
+                ContextStackAutoConfiguration.class,
+                ContextRegionProviderAutoConfiguration.class
+        }
+)
 @EnableFeignClients
 public class App {
 
